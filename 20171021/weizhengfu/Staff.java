a9828b1f94e33a15d9c9ff068e26e4e05036937f
weizhengfu <610721196@qq.com>
2017-10-21 23:49:59 +0800

    yyyy

---
+++ 20171021/weizhengfu/Staff.java
@@ -0,0 +1,23 @@
+public class Staff{
+	public String name;
+	public int age;
+	public String jop;
+	public String staff(){
+		return name;
+	}
+	public void staff(String name){
+		this.name = name;
+	}
+	public int age(){
+		return age;
+	}
+	public void age1(int age){
+		this.age = age;
+	}
+	public String jop(){
+		return jop;
+	}
+	public void jop1(String jop){
+		this.jop = jop;
+	}
+}
